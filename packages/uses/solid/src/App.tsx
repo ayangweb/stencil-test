d20ce439a99d85d28d262f4b69eb55d2d7c3af1a
ayang <473033518@qq.com>
2023-08-22 10:27:49 +0800

    update

--- packages/uses/solid/src/App.tsx
+++ packages/uses/solid/src/App.tsx
@@ -1,9 +1,6 @@
 import { MyComponent, MyTest } from "@stencil-test/solid";
-import { createSignal } from 'solid-js'
 
 function App() {
-	const [text, setText] = createSignal('test')
-
 	return (
 		<>
 			<MyComponent
@@ -18,9 +15,7 @@ function App() {
 			</MyComponent>
 
 			<MyTest slot="test">
-				{text()}
-
-				<input value={text()} onInput={ev => setText(ev.target.value)} />
+				<input />
 			</MyTest>
 		</>
 	);
