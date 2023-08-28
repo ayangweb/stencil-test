4b8d19c50f72f9dd5f3171afb69a1d495e7d1d19
ayang <473033518@qq.com>
2023-08-28 12:53:20 +0800

    update

--- packages/uses/solid/src/App.tsx
+++ packages/uses/solid/src/App.tsx
@@ -1,21 +1,30 @@
 import { MyComponent, MyTest } from "@stencil-test/solid";
+import { createSignal } from "solid-js";
 
 function App() {
+	const [count, setCount] = createSignal(1);
+
+	const changeCount = () => {
+		setCount(count() + 1);
+	};
+
 	return (
 		<>
 			<MyComponent
-				first="Solid"
-				onMyChange={() => {
-					console.log("Solid Trigger");
-				}}
+				first={String(count())}
+				onMyChange={changeCount}
 				style={{
 					color: "red",
 				}}
-			></MyComponent>
+			>
+				count {count()}
+			</MyComponent>
 
 			<MyTest slot="test">
 				<p>123</p>
 			</MyTest>
+
+			<button onClick={changeCount}>count {count()}</button>
 		</>
 	);
 }
