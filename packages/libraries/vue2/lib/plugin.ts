0531df9125a42eb514961c9c83fa16052a60bb2a
ayang <473033518@qq.com>
2023-09-09 10:31:35 +0800

    update

--- packages/libraries/vue2/lib/plugin.ts
+++ packages/libraries/vue2/lib/plugin.ts
@@ -1,8 +1,26 @@
+import type { PluginObject } from "vue";
 import { defineCustomElements, applyPolyfills } from "stencil-test/loader";
 
-const StencilTestUI = {
+const kebabCase = (eventName: string) => {
+	if (!eventName) return;
+
+	return eventName
+		.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2")
+		.toLowerCase();
+};
+
+const StencilTestUI: PluginObject<void> = {
 	install() {
-		applyPolyfills().then(() => defineCustomElements());
+		applyPolyfills().then(() =>
+			defineCustomElements(window, {
+				ael: (el: any, eventName: string, cb: any, opts: any) =>
+					el.addEventListener(kebabCase(eventName), cb, opts),
+				rel: (el: any, eventName: string, cb: any, opts: any) =>
+					el.removeEventListener(kebabCase(eventName), cb, opts),
+				ce: (eventName: string, opts: any) =>
+					new CustomEvent(kebabCase(eventName), opts),
+			} as any)
+		);
 	},
 };
 
